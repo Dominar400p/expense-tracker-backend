6f313073d5540877a4bc96c0f91033a58751912a
pradeep2037 <pradeep2037@gmail.com>
2026-06-21 22:36:28 +0530

    updated

--- app.py
+++ app.py
@@ -8,7 +8,14 @@ from routes.auth_routes import auth_bp
 app = Flask(__name__)
 
 # Enable CORS
-CORS(app, origins=["http://localhost:5173"])
+CORS(
+    app,
+    origins=[
+        "http://localhost:5173",
+        "https://expense-tracker-frontend-bc5t.onrender.com",
+    ],
+    supports_credentials=True,
+)
 
 # Register Blueprints
 app.register_blueprint(auth_bp)
